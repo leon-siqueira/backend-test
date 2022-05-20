0ca0ecc4b3407c785945d092a3877127cd1da701
Leon Siqueira <leon.siqueir4@gmail.com>
2022-05-20 14:40:53 -0300

    Delete Action.rb

--- level1/model/Action.rb
+++
@@ -1,9 +0,0 @@
-class Action
-  attr_reader :who, :type, :amount
-
-  def initialize(attributes = {})
-    @who = attributes[:who]
-    @type = attributes[:type]
-    @amount = attributes[:amount]
-  end
-end
